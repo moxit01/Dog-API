e25d8e410159151f8094913e9d405b0211b096f1
Moxit <shah.moxit999@gmail.com>
2022-06-14 16:32:13 -0400

    Initial commit

---
+++ API_Pick/ViewController.swift
@@ -0,0 +1,28 @@
+//
+//  ViewController.swift
+//  API_Pick
+//
+//  Created by Cambrian on 2022-05-09.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+//        DogAPI_Helper.fetchData { printString in
+//           print(printString)
+ //   }
+        // Do any additional setup after loading the view.
+    }
+
+//    @IBAction func showDogData(_ sender: Any) {
+//        DogAPI_Helper.fetchData { printString in
+//           print(printString)
+//        }
+//    }
+    
+}
+
